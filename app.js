99d57c59e33615b8e2802a1bd6ada1a68bcbec19
Stu Innes <stu.innes@emhtrade.com>
2019-09-13 18:49:09 +1200

    first cut

---
+++ app.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+var can = require('socketcan');
+
+var channel = can.createRawChannel("can0", true);
+var logger = fs.createWriteStream('./log.txt', {
+  flags: 'a' // 'a' means appending (old data will be preserved)
+})
+
+// Log any message
+channel.addListener("onMessage", (msg) => { 
+  logger.write(msg + '/n'); } );
+
