b134ba54bf31f5318f816b1c399360a8a08b3050
Stu Innes <>
2022-01-06 22:09:20 +1300

    changed write to print to get individual lines out

--- sailmate/io/canbus.py
+++ sailmate/io/canbus.py
@@ -84,12 +84,12 @@ def read_canbus(filename=None):
                                        tmp_msg.arbitration_id,
                                        tmp_msg.dlc,
                                        tmp_msg.data)
-                sys.stdout.writelines(nmea_msg.convert_to_actisense())
+                print(nmea_msg.convert_to_actisense())
 
     else:
         with open(filename) as test_file:
             for line in test_file:
-                sys.stdout.write(line)
+                print(line)
 
 
 if __name__ == "__main__":
